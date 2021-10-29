658d10842cf1a2e310b79fc56f0e77141bf82029
Marco Velez <marcovelezdc@gmail.com>
2021-10-29 14:04:26 -0700

    Cambios en basicamente todo

--- CV2_Implementation.py
+++ CV2_Implementation.py
@@ -1,16 +1,29 @@
 from PIL import Image
 import cv2
 import glob
-
+from time import sleep
 directory = glob.glob('CaptchaDataset/*.jpg')
 
 # Funcion para procesar las imagenes
 def processing(path, title):
-    img = cv2.imread(path,0) # La imagen es convertida a una escala de blanco y negro
-    ret, thresh2 = cv2.threshold(img,225,255,cv2.THRESH_BINARY_INV)
-    imgPro = Image.fromarray(thresh2)
-    imgPro.save(f"CaptchaDatasetCV2/{title}") # Se guarda la imagen procesada con el mismo titulo
+    print(title)
+    img = cv2.imread(path, 0) # La imagen es convertida a una escala de blanco y negro
 
+    #image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY )
+    #window_name = 'Image'
+    # Displaying the image 
+
+    
+    cv2.imwrite("eje2.png", img)
+    
+    '''
+    ret, thresh2 = cv2.threshold(img,128,255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
+    imgPro = Image.fromarray(thresh2)
+    imgPro.save("ejemplo.png")
+    #imgPro.save(f"CaptchaDatasetCV2/{title}") # Se guarda la imagen procesada con el mismo titulo
+    '''
+
+'''
 # Iterar sobre las imagenes
 for image in directory:
     with open(image, 'rb') as file:
@@ -19,3 +32,8 @@ for image in directory:
         path = image.title()
         print(nombre)
         processing(path, nombre)
+
+
+'''
+
+processing('00s6Q5Sfv7.jpg', '00s6Q5Sfv7')
--- ProcessAndSegmentation.py
+++ ProcessAndSegmentation.py
@@ -1,32 +1,67 @@
 import cv2
+from PIL import Image
+import glob
+import os
+import numpy as np 
 
-img = cv2.imread('Captcha.PNG',0)
+def processing(path, title):
+    # Dimensiones por modificar
+    width = 40
+    height = 40
 
-(h, w) = img.shape[:2]
-image_size = h*w
-mser = cv2.MSER_create()
-mser.setMaxArea(round(image_size/2))
-mser.setMinArea(10)
+    img = cv2.imread(path,0)
 
-ret, thresh2 = cv2.threshold(img,219,255,cv2.THRESH_BINARY_INV)
-regions, rects = mser.detectRegions(thresh2)
+    (h, w) = img.shape[:2]
+    image_size = h*w
+    mser = cv2.MSER_create()
+    mser.setMaxArea(round(image_size/2))
+    mser.setMinArea(10)
 
-print(rects)
-sumW = 0
-sumH = 0
-for i in range(len(rects)):
-    sumW = sumW + rects[i][2]
-    sumH = sumH + rects[i][3]
+    #ret, thresh2 = cv2.threshold(img,128,255,cv2.THRESH_BINARY_INV)
+    ret, thresh2 = cv2.threshold(img,230,255,cv2.THRESH_BINARY_INV)
+    regions, rects = mser.detectRegions(thresh2)
 
-print(sumW / len(rects))
-print(sumH / len(rects))
+    print(rects)
+    print()
+    rectsList = rects.tolist()
+    c = 0
+    for rectangles in rectsList:
+        sizeR = rectangles[2] * rectangles[3]
+        rectsList[c].append(sizeR)
+        c += 1
+    rectsList.sort(key=lambda row: row[4], reverse=True) 
+    rectsList = rectsList[:10]
+    print(rectsList)
+    print()
+    rectsList.sort(key=lambda row: row[0], reverse=False)
+    print(rectsList)
+    print()
 
+    
+    # quitar los ultimos 4 y primero caracteres del titulo (\ y .png)
+    titulo = title[1:-4]
+    letras = list(titulo)
 
-rectsOrd = rects[rects[:, 0].argsort()]
+    # TODO LO DEL FOLDER
+    path = "CAPTCHAS_SEGMENTADOS/" + titulo
+    os.mkdir(path)
+    
+    cont = 0
+    for x,y,w,h,s in rectsList:
+        imgPro = Image.fromarray(thresh2[y:y+h,x:x+w])
+        imgPro = imgPro.resize((width, height), Image.ANTIALIAS)
+        imgPro.save("CAPTCHAS_SEGMENTADOS/" + str(titulo) + "/{}".format(str(cont) + '_' + letras[cont] + '.PNG'))
+        cont+=1
+    
 
-cont = 0
-for x,y,w,h in rectsOrd:
-    if(h > ((sumH / len(rectsOrd)) - 4)):
-        cv2.imwrite('{}.png'.format(cont), thresh2[y:y+h,x:x+w])
-        cont += 1
-
+directory = glob.glob('CaptchaDataset/*.jpg')
+conte = 0
+for image in directory:
+    if(conte < 1):
+        conte += 1
+        with open(image, 'rb') as file:
+            nombre = image.title() 
+            nombre = nombre.removeprefix('Captchadataset')
+            path = image.title()
+            print(nombre)
+            processing(path, nombre)
