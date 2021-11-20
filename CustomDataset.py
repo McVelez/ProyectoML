3b6456d4d7da8c18cfbdfd7d1748c9db51e5743f
Marco Velez <marcovelezdc@gmail.com>
2021-11-19 23:58:57 -0800

    Se comentaron todas las funciones y acciones realizadas

--- CustomDataset.py
+++ CustomDataset.py
@@ -6,41 +6,65 @@ import cv2
 
 class CaracterDataset(Dataset):
 
+    # Inicializacion de variables
     def __init__(self, annotations_file, image_dir, device):
-        self.annotations = pd.read_csv(annotations_file)
-        self.image_dir = image_dir
-        self.device = device
+        # Variable que contiene la metadata e información relevante para cada archivo de imagen preprocesado y segmentado
+        self.annotations = pd.read_csv(annotations_file) 
+        # Directorio en donde se tienen todas las carpetas e imagenes segmentadas y preprocesadas
+        self.image_dir = image_dir 
+        # Dispositivo por ser asignado en caso de tener CUDA disponible
+        self.device = device 
 
     # Para identificar la cantidad de archivos en el dataset
     def __len__(self):
         return len(self.annotations)
 
-    # GetItem para acceder items del dataset
+    # GetItem para acceder a items del dataset
     def __getitem__(self, index):
-        image_sample_path = self._get_image_sample_path(index)
+        # Obtener el path y etiqueta del archivo de cada imagen
+        image_sample_path = self._get_image_sample_path(index) 
         label = self._get_image_sample_label(index)
+
+        # Realizar una lectura de la imagen y transformarla a un tensor object (para poder utilizar el modelo implementado)
         img = cv2.imread(image_sample_path)
-        #print(f"Training {image_sample_path}")
         img = torch.from_numpy(img)
         img = torch.Tensor.float(img)
+
+        # Se asigna la informacion de cada imagen al dispositivo disponible (En caso de tener CUDA el procesamiento es mas rapido)
         img = img.to(self.device)
-        img = self._mix_down_if_necessary(img) # Reducir a un solo canal 
+
+        # Reducir la imagen a un solo canal (normalizacion)
+        img = self._mix_down_if_necessary(img) 
+
+        # Regresar como salida la imagen (tensor object) y su etiqueta correspondiente (string)
         return img, label
 
+    # Funcion para normalizar todas las imagenes a un solo canal
     def _mix_down_if_necessary(self, img):
+        # Si el canal de la imagen es mayor a uno entonces se realiza una normalizacion a un solo canal por medio de torch.mean
         if img.shape[0] > 1: 
             img = torch.mean(img, dim=0, keepdim=True)
         return img
 
+    # Funcion para obtener la direccion completa de cada imagen
     def _get_image_sample_path(self, index):
-        fold = f"fold{self.annotations.iloc[index, 2]}" # index para fila y columna 3 ya que es la columna de fold en el archivo csv
+        # Obtener la direccion del folder y del archivo en base a la informacion almacenada en el archivo csv
+        fold = f"fold{self.annotations.iloc[index, 2]}" 
         filename = self.annotations.iloc[index, 3]
+
+        # Unir los paths del folder y del nombre del archivo para obtener la dirección completa, para asi poder acceder adecuadamente a todos los archivos almacenados
         path = os.path.join(self.image_dir, fold, filename)
+
+        # Regresar la direccion completa en donde se encuentra cada imagen
         return path
 
+    # Funcion para obtener la etiqueta correspondiente a cada archivo de imagen
     def _get_image_sample_label(self, index):
+        # Obtener y regresar la etiqueta correspondiente en funcion del archivo csv que contiene la metadata e informacion necesaria
         return self.annotations.iloc[index, 2] # labels = [0 = 0, 1=1, ..., 10 = a, ..., 61 = Z]
 
+
+# Prueba para identificar que las funciones descritas funcionen correctamente
 if __name__ == "__main__":
     ANNOTATIONS_FILE = "Caracteres_Metadata.csv"
     IMAGE_DIR = "CARACTERES"
@@ -54,4 +78,3 @@ if __name__ == "__main__":
     usd = CaracterDataset(ANNOTATIONS_FILE, IMAGE_DIR, device)
     print(f"There are {len(usd)} samples in the dataset.")
     img, label = usd[10000]
-    #img.show()
